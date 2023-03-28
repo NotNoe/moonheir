e9b2d26ffd8f53d6089b45e05ec69a0e4c4381f7
erik <erikzubi@ucm.es>
2023-03-28 23:13:38 +0200

    github pages done

--- gradient.js
+++ gradient.js
@@ -1,8 +1,12 @@
-const [red, green, blue] = [69, 111, 225]
+const [red, green, blue] = [255, 255, 255]
 const section1 = document.querySelector('body')
 
+//70,24,97
+
 window.addEventListener('scroll', () => {
-  const y = 1 + (window.scrollY || window.pageYOffset) / 150
-  const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+  const y_r = 1 + (window.scrollY || window.pageYOffset) / 175
+  const y_b = 1 + (window.scrollY || window.pageYOffset) / 250
+  const y_g = 1 + (window.scrollY || window.pageYOffset) / 150
+  const [r, g, b] = [red/y_r, green/y_g, blue/y_b].map(Math.round)
   section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
 })
