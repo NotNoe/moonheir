5747e1570c22c96058b7a7c41b26d69de38333fc
Elena <elennovi@ucm.es>
2023-05-02 22:26:33 +0200

    world2 bien importado

--- src/scenes/worldScenes/World2/World2_1.js
+++ src/scenes/worldScenes/World2/World2_1.js
@@ -2,7 +2,7 @@ import WorldScene from '../worldScene.js'
 
 export default class World2_1 extends WorldScene{
     constructor(){
-        super('World2_1', 'tilemapWorld2_1', 'tileset3');
+        super('World2_1', 'tilemapWorld2_1', 'pixil_tileset_3');
     }
 
     create(){
--- src/scenes/worldScenes/World2/World2_4.js
+++ src/scenes/worldScenes/World2/World2_4.js
@@ -2,7 +2,7 @@ import WorldScene from '../worldScene.js'
 
 export default class World2_4 extends WorldScene{
     constructor(){
-        super('World2_4', 'tilemapWorld2_4', 'tileset3');
+        super('World2_4', 'tilemapWorld2_4', 'pixil_tileset_3');
     }
 
     create(){
